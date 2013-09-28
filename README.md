128901743b801dec7a445ace19f6c163dfe7273a
Cory G Watson <gphat@onemogin.com>
2013-09-28 10:41:52 -0700

    Fix unnecessary debugging.

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ bits as needed or as patches arrive.
 
 ```
 // Add the Dep
-libraryDependencies += "wabisabi" %% "wabisabi" % "2.0.6"
+libraryDependencies += "wabisabi" %% "wabisabi" % "2.0.7"
 
 // And a the resolver
 resolvers += "gphat" at "https://raw.github.com/gphat/mvn-repo/master/releases/",
--- src/main/scala/wabisabi/Client.scala
+++ src/main/scala/wabisabi/Client.scala
@@ -325,7 +325,7 @@ class Client(esURL: String) extends Logging {
    */
   private def doRequest(req: Req) = {
     val breq = req.toRequest
-    error("%s: %s".format(breq.getMethod, breq.getUrl))
+    debug("%s: %s".format(breq.getMethod, breq.getUrl))
     Http(req.setHeader("Content-type", "application/json; charset=utf-8"))
   }
 }
