6ec60d1c99356aa4eb9466361e8fcdcf897f73e3
John Krah <john.krah@socrata.com>
2015-03-07 08:54:22 -0800

    doc ignoreConflicts

--- src/main/scala/wabisabi/Client.scala
+++ src/main/scala/wabisabi/Client.scala
@@ -299,6 +299,7 @@ class Client(esURL: String) extends Logging {
    * @param indices A sequence of index names for which mappings will be added.
    * @param type The type name to which the mappings will be applied.
    * @param body The mapping.
+   * @param ignoreConflicts When merge has conflicts overwrite mapping anyway, default false.
    */
   def putMapping(indices: Seq[String], `type`: String, body: String, ignoreConflicts: Boolean = false): Future[Response] = {
     val req = (url(esURL) / indices.mkString(",") / `type` / "_mapping").
